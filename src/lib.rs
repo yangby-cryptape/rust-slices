8c1d88867f4d579bbff791fbe674154a5d30dfd1
Boyu Yang <yangby@cryptape.com>
2019-05-18 16:06:55 +0800

    feat: add a simple implementation to construct u8 slices

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,34 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+
+//! A series of macros that used to construct static unsigned integer slices from string literals.
+//!
+//! # Usage
+//!
+//! The input is a hexadecimal string literal with `0x` prefix.
+//! The size of input should be a even number.
+//!
+//! And you can use any number of `_` in the string literal to separate it for more readable.
+//!
+//! ## Examples
+//!
+//! ```rust
+//! use slices::u8_slice;
+//!
+//! const VAL: &[u8] = u8_slice!("0x_1234_5678_9abc_def0");
+//! const NULL: &[u8] = u8_slice!("0x");
+//!
+//! fn main () -> ::std::io::Result<()> {
+//!     let val = &[0x12u8, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0];
+//!     assert_eq!(VAL, val);
+//!     assert_eq!(NULL, &[]);
+//!     Ok(())
+//! }
+//! ```
+
+use proc_macro_hack::proc_macro_hack;
+
+/// A macro used to construct static u8 slices from string literals.
+#[proc_macro_hack]
+pub use hack::u8_slice;
